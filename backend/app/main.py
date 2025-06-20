8913444b400fad83faffa5c35d8dfcbef6426f8a
Akanksh Borah <akankshborah@gautams-mbp.lan>
2025-06-20 16:15:00 -0700

    1st parts of CRUD

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,17 +1,6 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from .api.v1.endpoints import users
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-@app.get("/")
-def root():
-    return {"message": "Hello from FastAPI!"}
-
+app.include_router(users.router)
