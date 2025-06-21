435350c0876e5528caff14a9013b53fead15927b
Akanksh Borah <akankshborah@gautams-mbp.lan>
2025-06-21 13:53:17 -0700

    Added Email validation

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 from typing import Optional
 from uuid import UUID
 ## User Class
 class User(BaseModel):
     id: Optional[UUID] = None
     name: str
-    email: str
+    email: EmailStr
     age: int
