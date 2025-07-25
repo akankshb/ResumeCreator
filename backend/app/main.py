c356a78c2106b83f67b8afb225bd34f52f9c9cb6
Akanksh Borah <akankshborah@gautams-mbp.lan>
2025-07-24 22:56:53 -0700

    Start of linking frontend and backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
 from .api.v1.endpoints import users
+from fastapi.middleware.cors import CORSMiddleware 
 
 app = FastAPI()
 
 app.include_router(users.router)
+
+# Allow frontend origin
+origins = [
+    "http://localhost:3000",  # React dev server
+    "http://127.0.0.1:3000",
+    # Add your production frontend URL here when deploying
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,          # Whitelist frontends
+    allow_credentials=True,
+    allow_methods=["*"],            # GET, POST, PUT, etc.
+    allow_headers=["*"],
+)
--- frontend/react_interface/src/pages/FormQuestions.js
+++ frontend/react_interface/src/pages/FormQuestions.js
@@ -1,13 +1,25 @@
 import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import "./FormQuestions.css"
+import axios from 'axios'
+
 function Form() {
     const [name, setName] = useState("");
     const [age, setAge] = useState("");
+    axios.get("http://127.0.0.1:8000")
+    .then(response => {
+        console.log(response.status);
+      console.log(response.data);
+    })
+    .catch(function (error) {
+        console.log("Hello World1")
+        console.error(error);
+      });    
     return (
         <div className="App">
         <h1>Resume Builder</h1>
-        <p>Explanation for website use needed*</p>
+        <p>{}</p>
+        {/* <p>Explanation for website use needed*</p>
         <form>
             <label>1. Enter your full name: 
             <br></br>
@@ -22,13 +34,18 @@ function Form() {
                 <br/> 
             <input 
                 type="number" 
-                name="age" 
+                name="age"
+                min = "12"
+                max = "120"  
                 value={age} 
                 onChange={(e) => setAge(e.target.value)}
             />
             </label>
         </form>
-        
+        <button onClick={
+            () => alert("This button works") 
+        }>Create Account</button>
+         */}
         </div>
         
     );
